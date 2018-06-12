b44f877baa55134b86db505c1481773e7bade9bf
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-06-12 20:31:03 +0530

    Counter now working .

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,18 +52,45 @@ $(document).ready(function() {
     /* --------------------------------------------------------
      COUNTER JS
      ----------------------------------------------------------- */
+     function makeTimer() {
 
-    $('.counter').counterUp({
-        delay: 5,
-        time: 3000
-    });
+        var endTime = new Date("27 October 2018 9:56:00 GMT+01:00");			
+        endTime = (Date.parse(endTime) / 1000);
 
-    $(".countdown")
-        .countdown("2018/03/01", function(event) {
-            $(this).html(
-                event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
-            );
-        });
+        var now = new Date();
+        now = (Date.parse(now) / 1000);
+
+        var timeLeft = endTime - now;
+
+        var days = Math.floor(timeLeft / 86400); 
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+        if (hours < "10") { hours = "0" + hours; }
+        if (minutes < "10") { minutes = "0" + minutes; }
+        if (seconds < "10") { seconds = "0" + seconds; }
+
+        $("#days").html( days + "<span>Days</span>");
+        $("#hours").html(hours +"<span>Hours</span>");
+        $("#minutes").html(minutes + "<span>Minutes</span>");
+        $("#seconds").html(seconds + "<span>Seconds</span>");		
+
+}
+
+setInterval(function() { makeTimer(); }, 1000);
+
+    // $('.counter').counterUp({
+    //     delay: 5,
+    //     time: 3000
+    // });
+
+    // $(".countdown")
+    //     .countdown("2018/03/01", function(event) {
+    //         $(this).html(
+    //             event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
+    //         );
+    //     });
 
     /* ==============================================
      SLIDER
